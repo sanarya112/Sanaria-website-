4972010dca3c3bed4db1a8dcc7d5fdc1c0ed9516
zhiwar404M <97038061+zhiwar404M@users.noreply.github.com>
2026-03-30 01:18:42 +0300

    Add Firebase configuration and initialization

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// ── FIREBASE CONFIG ──
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, onSnapshot }
+  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7jIwGVdqII0v53gFw83mP9Pa4PBSLg2I",
+  authDomain: "sanush-website-datebase.firebaseapp.com",
+  projectId: "sanush-website-datebase",
+  storageBucket: "sanush-website-datebase.firebasestorage.app",
+  messagingSenderId: "336092110705",
+  appId: "1:336092110705:web:1b7b06b247ab1fae54e0d0",
+  measurementId: "G-3P54X84P0S"
+};
+
+const app      = initializeApp(firebaseConfig);
+const auth     = getAuth(app);
+const db       = getFirestore(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, db, provider, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, doc, setDoc, getDoc, onSnapshot };
