94961b16ff3cef5ee07c3fd8473bc07ff43f9c39
zhiwar404M <97038061+zhiwar404M@users.noreply.github.com>
2026-04-01 03:08:34 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -15,12 +15,13 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyC7jIwGVdqII0v53gFw83mP9Pa4PBSLg2I",
-  authDomain:        "sanush-website-datebase.firebaseapp.com",
-  projectId:         "sanush-website-datebase",
-  storageBucket:     "sanush-website-datebase.firebasestorage.app",
+  apiKey: "AIzaSyC7jIwGVdqIIOv53gFw83mP9Pa4PBSLg2I",
+  authDomain: "sanush-website-datebase.firebaseapp.com",
+  projectId: "sanush-website-datebase",
+  storageBucket: "sanush-website-datebase.firebasestorage.app",
   messagingSenderId: "336092110705",
-  appId:             "1:336092110705:web:1b7b06b247ab1fae54e0d0"
+  appId: "1:336092110705:web:1b7b06b247ab1fae54e0d0",
+  measurementId: "G-3P54X84P0S"
 };
 
 const app = initializeApp(firebaseConfig);
