4b7f73c9e726654a9f486196348c8e7f91f1ee68
zhiwar404M <97038061+zhiwar404M@users.noreply.github.com>
2026-04-01 04:30:26 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,16 @@
 // ── FIREBASE CONFIG ──
 (function () {
   var config = {
-    apiKey:            "AIzaSyC7jIwGVdqII0v53gFw83mP9Pa4PBSLg2I",
-    authDomain:        "sanush-website-datebase.firebaseapp.com",
-    projectId:         "sanush-website-datebase",
-    storageBucket:     "sanush-website-datebase.firebasestorage.app",
-    messagingSenderId: "336092110705",
-    appId:             "1:336092110705:web:1b7b06b247ab1fae54e0d0"
-  };
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7jIwGVdqIIOv53gFw83mP9Pa4PBSLg2I",
+  authDomain: "sanush-website-datebase.firebaseapp.com",
+  projectId: "sanush-website-datebase",
+  storageBucket: "sanush-website-datebase.firebasestorage.app",
+  messagingSenderId: "336092110705",
+  appId: "1:336092110705:web:1b7b06b247ab1fae54e0d0",
+  measurementId: "G-3P54X84P0S"
+};
 
   try {
     if (!firebase.apps.length) {
